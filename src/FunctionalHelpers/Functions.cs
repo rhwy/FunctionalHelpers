cacf27b72450dfe39465a8c2211ce09aec5de386
rhwy <github@rui.fr>
2017-04-22 11:40:28 +0200

    added options

--- src/FunctionalHelpers/Functions.cs
+++ src/FunctionalHelpers/Functions.cs
@@ -16,8 +16,50 @@ namespace FunctionalHelpers
 
 
 
-
+    
         public static U Then<T,U>(this T me, Func<T,U> next) => next(me);
         public static U Then<T,U>(this IEnumerable<T> me, Func<IEnumerable<T>,U> next) => next(me);
+    }
+
+
+    public interface Option<T>
+    {
+        bool HasValue {get;}
+    }
+
+    public static class OptionExtensions
+    {
+        public static None<T> None<T>(this T currentObject) 
+            => new None<T>();
+
+        public static T WithValue<T>(this Option<T> currentOption,
+            Func<T> none, Func<T,T> some)
+        {
+            switch (currentOption)
+            {
+                case Some<T> someValue:
+                    return some(someValue.Value);
+                default:
+                    return none();
+            }
+        }
+    }
+    public struct None<T> : Option<T>
+    {
+        public bool HasValue => false;
+        public static None<T> New => new None<T>();
+    }
+
+    public struct Some<T> : Option<T>
+    {
+        public T Value {get;}
+
+        public bool HasValue => true;
+        private Some(T value) => 
+            Value = EqualityComparer<T>.Default.Equals(value, default(T))
+            ? throw new InvalidOperationException("value can't be null")
+            : value ;
+
+        public static Some<T> NewValue(T value) => new Some<T>(value);
     }
 }
--- test/FunctionalHelpers.Tests/SimplifyFunctionsWriting.cs
+++ test/FunctionalHelpers.Tests/SimplifyFunctionsWriting.cs
@@ -33,4 +33,21 @@ namespace FunctionalHelpers.Tests
 
         }
     }
+
+    public class UseOptionsForBetterTypes
+    {
+        [Fact]
+        public void use_options()
+        {
+            Option<int> one = None<int>.New;
+            Option<int> two = Some<int>.NewValue(42);
+
+            int value = one.WithValue(
+                none : ()=> 0,
+                some : i => i);
+            
+            Assert.Equal(0,value);
+        }
+    }
+    
 }
