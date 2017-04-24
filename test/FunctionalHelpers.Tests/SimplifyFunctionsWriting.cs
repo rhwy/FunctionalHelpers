4c6dd15dcef03da9284b2d3dc7e732beef5bc42b
rhwy <github@rui.fr>
2017-04-24 10:21:20 +0200

    added railway, tests refactored to files

--- test/FunctionalHelpers.Tests/SimplifyFunctionsWriting.cs
+++ test/FunctionalHelpers.Tests/SimplifyFunctionsWriting.cs
@@ -1,11 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Xunit;
-using static FunctionalHelpers.Core;
-
 namespace FunctionalHelpers.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+    using static FunctionalHelpers.Core;
+
+
     public class SimplifyFunctions
     {
         [Fact]
@@ -34,20 +35,9 @@ namespace FunctionalHelpers.Tests
         }
     }
 
-    public class UseOptionsForBetterTypes
-    {
-        [Fact]
-        public void use_options()
-        {
-            Option<int> one = None<int>.New;
-            Option<int> two = Some<int>.NewValue(42);
+    
+    
+    
 
-            int value = one.WithValue(
-                none : ()=> 0,
-                some : i => i);
-            
-            Assert.Equal(0,value);
-        }
-    }
     
 }
